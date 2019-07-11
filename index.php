98b3313af0d96052316faf273d94384a9e066728
Fred Bradley <hello@fredbradley.co.uk>
2019-07-11 17:24:29 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+  <head>
+<link rel="shortcut icon" href="assets/CirclePhoto.png" 
+type="image/x-icon">
+	<title>Fred &amp; Louise</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+  <meta name="description" content="Fred and Louise. A media nerd from a boarding school run by monks. After a year with some Australians, found love whilst cooking some chicken on a BBQ dressed in rugby shorts and ugg boots. And A girl who works hard and plays hard, with the love of all furry things with four legs! Seemingly temporary blind in 2006, seeing past the rugby shorts and ugg boots – gone but not forgotten!">
+<meta name="robots" content="index, follow">
+
+	  
+	<link href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" media="screen">
+
+	<style>
+		  body {
+			  background-color: #5B415F !important;
+			  
+		  }
+		  #about-row {
+			  background-image: url('assets/flbg.png');
+			  color:#fff !important;
+			  padding:20px;
+		  }
+		  #about-row .img-circle {
+			  border-color:#fff;
+			  border-width: 2px;
+			  border-style: solid;
+		  }
+		  body {
+			  background-image: url('assets/P209.jpg');
+			  background-size: cover;
+		  }
+		  a {
+			  color:#fff !important;
+			  text-decoration: underline !important;
+		  }
+		  span.microdata {
+			  display: none;
+		  }
+	</style>
+    
+    <!-- Bootstrap core CSS -->
+ 
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js"></script>
+    <![endif]-->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-24018806-23"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-24018806-23');
+</script>
+
+  </head>
+  <body>
+<div class="" id="church-bg">
+	<div class="clearfix" style="padding-top:100px; padding-bottom:100px;">
+		<div class="container">
+			<div class="row img-rounded" id="about-row">
+				<div class="col-md-6" itemscope itemtype="http://schema.org/Person">
+					<span class="microdata" itemprop="name">Fred Bradley</span>
+					<span class="microdata" itemprop="spouse">Louise Bradley</span>
+
+					<div class="row">
+						<div class="col-xs-5 pull-right">
+							<img itemprop="image" alt="Photo of Fred as a baby" class="img-circle img-responsive" src="assets/FredBabyCropped.png" />
+						</div>
+
+						<div class="col-xs-7">
+							<h2>About Fred</h2>
+							<p>A media nerd from a boarding school run by monks. After a year with some Australians, found love whilst cooking some chicken on a BBQ dressed in rugby shorts and ugg boots!</p>
+							<p><a itemprop="url" target="_blank" href="https://www.fredbradley.uk">More Fred</a></p>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-6" itemscope itemtype="http://schema.org/Person">
+					<span class="microdata" itemprop="name">Louise Bradley</span>
+					<span class="microdata" itemprop="spouse">Fred Bradley</span>
+					<div class="row">
+						<div class="col-xs-5">
+							<img itemprop="image" alt="Photo of Louise as a baby" class="img-circle img-responsive" src="assets/LouiseBabyCropped.png" />
+						</div>
+						<div class="col-xs-7">
+							<h2>About Louise</h2>
+							<p>A girl who works hard and plays hard, with the love of all furry things with four legs! Seemingly temporary blind in 2006, seeing past the rugby shorts and ugg boots – gone but not forgotten!</p>
+							<p><a itemprop="url" target="_blank" href="http://www.louisebradley.me">More Louise</a></p>
+						</div>
+					</div>
+				</div>
+		</div>
+	</div>
+</div>
+    
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  </body>
+</html>
